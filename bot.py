fa768ef2ee2600963efb1d2f81f0973f0b46c669
Hajisoy Mikayil <mikayilhajisoy@gmail.com>
2024-08-17 10:53:21 +0400

    New Handler

--- bot.py
+++ bot.py
@@ -22,8 +22,12 @@ I am here to echo your kind words back to you. Just say anything nice and I'll s
 def echo_message(message):
     bot.reply_to(message, message.text)
 
+@bot.message_handler(content_types=['new_chat_members'])
+def make_some(message):
+    bot.send_message(message.chat.id, 'I accepted a new user!')
+    bot.approve_chat_join_request(message.chat.id, message.from_user.id)
 
-@bot.message_handler(commands=['help', 'start'])
+@bot.message_handler(commands=['info'])
 def send_welcome(message):
     bot.reply_to(message, """\
 Hi there, I am a Bot.
